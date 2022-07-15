701fc1fa4d1dda6abbc804086caf01b62396a0ee
KorneliaAL <104372863+KorneliaAL@users.noreply.github.com>
2022-07-15 23:08:48 +0200

    Showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+function displayTemperature(response) {
+  let city = document.querySelector("#city");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+  let temperature = document.querySelector("#current-temp");
+  city.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  temperature.innerHTML = Math.round(response.data.main.temp);
+}
 
+let apiKey = "70f46e7b374a41958649bba5f746057f";
+let urlApi = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+axios.get(urlApi).then(displayTemperature);
